7c255c82719db77368a05f1588cce7596bb6dddc
Michael Parkes <55591861+mdparkes@users.noreply.github.com>
2024-12-02 22:40:14 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,7 @@
+// Create web server
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+var comments = [];
